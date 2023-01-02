0e7c2384e7e5a9ddf3aef7301b143de50c48b1a7
raed_nb@hotmail.com <raed_nb@hotmail.com>
2023-01-02 13:02:08 +0200

    navigate back to login if token not stored

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
 import Home from "./components/Home";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <div className="App">
       <Routes>
