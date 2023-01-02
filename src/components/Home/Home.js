d1326f7523f65d49033f54489b03146b4c0334c1
raed_nb@hotmail.com <raed_nb@hotmail.com>
2023-01-02 16:18:14 +0200

    add navigation to product on product selection

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,29 +5,15 @@ import MenuItem from "@mui/material/MenuItem";
 import axios from "axios";
 import { BASE_API_URL } from "../../constants/Paths";
 import { Avatar, CircularProgress, Pagination } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Header from "../Header";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   const [isLoading, setIsLoading] = useState(false);
-  const [categoryList, setCategoryList] = useState([]);
   const [productList, setProductList] = useState([]);
   const [productsLimit, setProductsLimit] = useState(10);
-
-  useEffect(() => {
-    loadCategories();
-  }, []);
-
-  const loadCategories = () => {
-    setIsLoading(true);
-    axios
-      .get(`${BASE_API_URL}/products/categories`)
-      .then(function (response) {
-        setIsLoading(false);
-        setCategoryList(response.data);
-      })
-      .catch(function (error) {
-        setIsLoading(false);
-      });
-  };
 
   useEffect(() => {
     loadProducts();
@@ -47,6 +33,10 @@ const Home = () => {
       });
   };
 
+  const selectProduct = (productId) => {
+    navigate(`/product/${productId}`);
+  };
+
   return (
     <div
       style={{
@@ -56,100 +46,7 @@ const Home = () => {
         // position: "fixed",
       }}
     >
-      <div className="row">
-        <div
-          style={{ marginTop: "44px", marginLeft: "20px" }}
-          className="col-4"
-        >
-          {" "}
-          <img width={"111.58px"} height="24px" src="home-logo.png"></img>
-        </div>
-        <div
-          style={{
-            marginTop: "30px",
-            // marginLeft: "476.58px",
-            // position: "fixed",
-          }}
-          className="col-4"
-        >
-          <img
-            width={"488.86px"}
-            height="52px"
-            src="search-bar.png"
-            alt="search-bar"
-          ></img>
-        </div>
-        <div
-          style={{
-            marginLeft: "1306.45px",
-            position: "relative",
-            bottom: "35px",
-          }}
-          className="col-2"
-        >
-          <img width={"22px"} height="22px" src="User.png"></img>
-        </div>
-        <div
-          style={{
-            marginLeft: "1342.45px",
-            position: "relative",
-            bottom: "60px",
-          }}
-          className="col-2"
-        >
-          <img width={"28px"} height="24px" src="cart.png"></img>
-        </div>
-      </div>
-      <div className="row">
-        <div
-          className="col-3"
-          style={{
-            marginTop: "40px",
-            marginLeft: "100px",
-            position: "absolute",
-          }}
-        >
-          <img width={"141px"} height="31px" src="category.png"></img>
-        </div>
-        <div
-          className="col-9"
-          style={{
-            // position: "fixed",
-            marginTop: "40px",
-            marginLeft: "40px",
-            marginRight: "50px",
-          }}
-        >
-          {categoryList.length == 0 ? (
-            <CircularProgress />
-          ) : (
-            categoryList.map((category) => {
-              return (
-                <div
-                  style={{
-                    display: "inline-block",
-                    position: "relative",
-                    // padding: "15px 32px",
-                    textAlign: "center",
-                    left: "200px",
-                    // position: "absolute",
-                  }}
-                >
-                  <Button
-                    // id="basic-button"
-                    style={{
-                      color: "white",
-                    }}
-                    // aria-haspopup="true"
-                  >
-                    {category}
-                  </Button>
-                </div>
-              );
-            })
-          )}
-        </div>
-      </div>
+      <Header />
       <div className="row" style={{ marginTop: "10px", marginLeft: "90px" }}>
         {productList.length == 0 ? (
           <CircularProgress />
@@ -163,7 +60,12 @@ const Home = () => {
                   alt="Remy Sharp"
                   src={product.image}
                 /> */}
-                <img width={"228px"} height="330px" src={product.image}></img>
+                <img
+                  onClick={() => selectProduct(product.id)}
+                  width={"228px"}
+                  height="330px"
+                  src={product.image}
+                ></img>
                 <div
                   style={{
                     display: "flex",
