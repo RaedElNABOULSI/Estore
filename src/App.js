2efc869fd0d78d1544f10391d160c1552a868e16
raed_nb@hotmail.com <raed_nb@hotmail.com>
2023-01-02 15:51:38 +0200

    add path for specified product

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import Product from "./components/Product";
 
 function App() {
   const navigate = useNavigate();
@@ -19,6 +19,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
         {/* <Route path="*" element={<Navigate to={"/404"} />} /> */}
       </Routes>
     </div>
